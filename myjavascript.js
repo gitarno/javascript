be035ebd4890891194008d07a7016fbe2dfbf65c
Arno <arno@advg.com.br>
2014-12-23 21:52:28 -0200

    Orientações por evento!

--- myjavascript.js
+++ myjavascript.js
@@ -1,34 +1,40 @@
-// 
-
-function ativarDiv(elemento){
-	elemento.addEventListener("click", function(){
-	    var conteudo = this.innerHTML;
-			var h1 = document.createElement("h1");
-			h1.innerHTML = conteudo;
-
-			this.parentNode.insertBefore(h1,this);
-			this.parentNode.removeChild(this);
-	});
+   
+var resultado = new Number();
+/**
+ * [calcular description]
+ * @return {[type]} [description]
+ */
+function calcular(){
+  var v1 = new Number(document.getElementsByClassName("valor")[0].value);
+  var v2 = new Number(document.getElementsByClassName("valor")[1].value);
+  resultado = somar(v1, v2);
+}   
+/**
+ * [somar description]
+ * @param  {[type]} numeroA [description]
+ * @param  {[type]} numeroB [description]
+ * @return {[type]}         [description]
+ */
+function somar(numeroA, numeroB){
+  var retorno = numeroA + numeroB;
+  return retorno;
 }
+/**
+ * [imprimir description]
+ * @return {[type]} [description]
+ */
+function imprimir(){
+  document.getElementById("resultado").innerHTML = resultado;
+}  
+/**
+ * [onload description]
+ * @return {[type]} [description]
+ */
+window.onload = function(){
+  document.getElementById('calcular').addEventListener("click", function(){
+    calcular();
+    imprimir();
+  }); 
+} 
 
-
-window.onload=function(){
-	
-	var divs = document.getElementsByTagName('div');
-	//divs[0].innerHTML = "TEstando";
-	divs[0].id = "TEstando";
-	divs[0].className = "TEstando";
-
-	for(x in divs){
-		ativarDiv(divs[x]);
-	}
-
-	document.getElementsByClassName('addTelefone')[0].addEventListener("click", function(){
-			var input = document.createElement("input");
-			input.type = 'number'
-			this.parentNode.insertBefore(input,this);
-
-	});
-
-
-}
+// <body onload="escrever();">

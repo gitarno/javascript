d8449be1662a61e03a633d9784b2ee1201f85a5f
Arno <arno@advg.com.br>
2015-10-08 14:27:35 -0300

    Udapte Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,16 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-      	pkg: grunt.file.readJSON('package.json'),
-      	uglify: {
+      pkg: grunt.file.readJSON('package.json'),
+      uglify: {
         dist: {
-			options: {
-			  mangle: true,
-			  compress: true
-        	},
-	        files: {
-	          'calc-pattern/**.min.js': [calc-pattern/**/*.js,!calc-pattern/**/*.min.js]
-	        }
-      	}
+  			  options: {
+  				  compress: true
+          },
+          files: {
+            'calc-pattern/**.min.js': [ "calc-pattern/**/*.js" , "!calc-pattern/**/*.min.js"]
+          }
+       }
     }
   });
 

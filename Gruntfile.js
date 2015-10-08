e7cdd00933d82c4f2fbfaab16a8fb0a27f31fc0f
Arno <arno@advg.com.br>
2015-10-08 14:20:53 -0300

    delete exception token .

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,21 @@
 module.exports = function (grunt) {
 
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      uglify: {
+      	pkg: grunt.file.readJSON('package.json'),
+      	uglify: {
         dist: {
-			  options: {
-       	  banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-				  mangle: true,
-				  compress: true
-        },
-        files: {
-          'calc-pattern/**.min.js': [calc-pattern/**/*.js,!calc-pattern/**/*.min.js];
-        }
-      }
-    });
+			options: {
+			  mangle: true,
+			  compress: true
+        	},
+	        files: {
+	          'calc-pattern/**.min.js': [calc-pattern/**/*.js,!calc-pattern/**/*.min.js];
+	        }
+      	}
+    }
+  });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.registerTask('default', ['uglify']);
   
-  };
+};
